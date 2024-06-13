51ca5a00cae35ccb84ef9c2a7bb50907ba6966fd
Wesley <85897421+w3ssfs@users.noreply.github.com>
2024-06-12 21:07:08 -0300

    Add locale

---
+++ script-crud.js
@@ -0,0 +1,16 @@
+const btnAdicionarTarefa = document.querySelector(".app__button--add-task");
+const formAdicionarTarefa = document.querySelector(".app__form-add-task");
+const textArea = document.querySelector(".app__form-textarea");
+
+const tarefas = [];
+
+btnAdicionarTarefa.addEventListener("click", () => {
+  formAdicionarTarefa.classList.toggle("hidden");
+});
+
+formAdicionarTarefa.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const tarefa = { descricao: textArea.value };
+  tarefas.push(tarefa);
+  localStorage.setItem("tarefas", tarefas);
+});
