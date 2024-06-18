859ffc649962d6cb83a23c68cce3c70fe774f87d
Wesley <85897421+w3ssfs@users.noreply.github.com>
2024-06-17 21:54:58 -0300

    retorn localstore

--- script-crud.js
+++ script-crud.js
@@ -4,6 +4,32 @@ const textArea = document.querySelector(".app__form-textarea");
 
 const tarefas = [];
 
+function criarTarefa(tarefa) {
+  const li = document.createElement("li");
+  li.classList.add("app__section-task-list-item");
+
+  const svg = document.createElement("svg");
+  svg.innerHTML = `<svg>
+        <svg class="app__section-task-icon-status" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <circle cx="12" cy="12" r="12" fill="#FFF"></circle>
+            <path d="M9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.4062L4.82812 12L9 16.1719Z" fill="#01080E"></path>
+        </svg>
+    </svg>`;
+
+  const paragrafo = document.createElement("p");
+  paragrafo.textContent = tarefa.descricao;
+
+  const botao = document.createElement("button");
+  const imagemBotao = document.createElement("img");
+
+  imagemBotao.setAttribute("src", "/imagens/edit.png");
+
+  botao.append(imagemBotao);
+  li.append(svg);
+  li.append(paragrafo);
+  li.append(botao);
+}
+
 btnAdicionarTarefa.addEventListener("click", () => {
   formAdicionarTarefa.classList.toggle("hidden");
 });
@@ -12,5 +38,5 @@ formAdicionarTarefa.addEventListener("submit", (e) => {
   e.preventDefault();
   const tarefa = { descricao: textArea.value };
   tarefas.push(tarefa);
-  localStorage.setItem("tarefas", tarefas);
+  localStorage.setItem("tarefas", JSON.stringify(tarefas));
 });
